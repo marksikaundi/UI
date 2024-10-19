43afee0c0729985e01ddd2044871c71fc336b91f
marksikaundi <emmanuelmark117@gmail.com>
2024-10-19 05:31:05 +0200

    Refactor NotFound component: Update 404 page with program redirect buttons and contact option

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,7 @@
 "use client";
-import Link from "next/link";
+
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { ChevronRight } from "lucide-react";
 import { useRouter } from "next/navigation";
 
 export default function NotFound() {
@@ -11,10 +10,14 @@ export default function NotFound() {
     router.push("/programs");
   };
 
+  const handleContact = () => {
+    router.push("/contact");
+  };
+
   return (
     <div className="min-h-screen bg-black flex flex-col items-center justify-center px-4 text-center">
       <div className="space-y-8">
-        <div className="w-full items-center flex flex-col mb-4">
+        <div className="w-full items-center flex flex-col mt-8 mb-4">
           <Image src="/assets/404.svg" width={300} height={300} alt="404" />
         </div>
         <div className="items-center ">
@@ -25,27 +28,39 @@ export default function NotFound() {
             You may have mistyped the address or the page may have moved.
           </p>
         </div>
-        <div className="mt-10 sm:flex sm:justify-center lg:justify-center">
-          <div className="rounded-md shadow">
+
+        <section className="py-8 text-center">
+          <div className="flex justify-center space-x-4 mb-16">
             <Button
               onClick={handleRedirect}
               variant="default"
               size="lg"
               className="rounded-full bg-white hover:bg-white text-black"
             >
-              Programs <ChevronRight className="ml-2 h-5 w-5" />
+              Go to Programs
             </Button>
-          </div>
-          <div className="mt-3 sm:mt-0 sm:ml-3">
             <Button
-              onClick={handleRedirect}
+              onClick={handleContact}
               size="lg"
               className="rounded-full hover:rounded-full text-white bg-black hover:bg-[#1F2225]"
             >
-              Contact us <ChevronRight className="ml-2 h-5 w-5 " />
+              Contact Us
             </Button>
           </div>
-        </div>
+          <div className="relative">
+            <div className="absolute inset-0 bg-gradient-to-b from-black to-transparent" />
+            <Image
+              src="/lupleg.svg"
+              height="200"
+              width="600"
+              alt="Lupleg"
+              className="mx-auto"
+              style={{
+                filter: "drop-shadow(0 0 20px rgba(255,255,255,0.2))",
+              }}
+            />
+          </div>
+        </section>
       </div>
     </div>
   );
