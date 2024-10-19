e456e1d82f35c0a34521c11a5742589862d08af8
marksikaundi <emmanuelmark117@gmail.com>
2024-10-19 04:53:44 +0200

    Refactor PsCard component: Update program details and images

--- app/programs/ps-card.tsx
+++ app/programs/ps-card.tsx
@@ -1,15 +1,16 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function PsCard() {
   return (
     <div className="min-h-screen  text-white p-4 sm:p-6 md:p-8 flex flex-col items-center">
       <div className="w-full max-w-4xl">
-        <Card className="bg-[#13132B] my-8 border-gray-700">
+        <Card className="bg-[#18181B] my-8 border-gray-700">
           <CardContent className="p-4 sm:p-6">
             <div className="flex flex-col sm:flex-row items-center">
-              <img
+              <Image
                 src="/ps/dev.svg"
                 alt="Software Engineering at Lupleg"
                 className="mb-4 sm:mb-0 sm:mr-6 w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32"
@@ -19,20 +20,19 @@ export default function PsCard() {
               <div>
                 <div className="flex flex-col sm:flex-row items-center sm:items-start mb-2">
                   <h2 className="text-xl sm:text-2xl text-white font-bold mb-2 sm:mb-0 sm:mr-3 text-center sm:text-left">
-                    Software Engineering Essentials
+                    Web Technologies Pro
                   </h2>
-                  {/* <span className="bg-green-500 text-black text-xs font-bold px-2 py-1 rounded">
-                  </span> */}
                 </div>
                 <p className="text-green-400 text-sm mb-3 text-center sm:text-left">
-                  6 Months Program
+                  2 Months Program
                 </p>
                 <p className="text-sm text-gray-300 mb-4 text-center sm:text-left">
-                  A complete practical course to become a software developer.
-                  From learning basic programming to building softwares. 
+                  Total Web Technologies Pro is the ultimate guide to becoming a
+                  web technologies wizard. Learn how to build web applications
+                  in a professional way.
                 </p>
                 <div className="flex justify-center sm:justify-start">
-                  <Link href="/programs/se">
+                  <Link href="/programs/">
                     <Button variant="secondary" size="sm">
                       Learn More →
                     </Button>
@@ -43,10 +43,10 @@ export default function PsCard() {
           </CardContent>
         </Card>
 
-        <Card className="bg-[#13132B] my-8 border-gray-700">
+        <Card className="bg-[#18181B] my-8 border-gray-700">
           <CardContent className="p-4 sm:p-6">
             <div className="flex flex-col sm:flex-row items-center">
-              <img
+              <Image
                 src="/ts.webp"
                 alt="TypeScript logo with glowing effect"
                 className="mb-4 sm:mb-0 sm:mr-6 w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32"
@@ -81,10 +81,10 @@ export default function PsCard() {
           </CardContent>
         </Card>
 
-        <Card className="bg-[#13132B] my-8 border-gray-700">
+        <Card className="bg-[#18181B] my-8 border-gray-700">
           <CardContent className="p-4 sm:p-6">
             <div className="flex flex-col sm:flex-row items-center">
-              <img
+              <Image
                 src="/ps/re.svg"
                 alt="TypeScript logo with glowing effect"
                 className="mb-4 sm:mb-0 sm:mr-6 w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32"
@@ -119,10 +119,10 @@ export default function PsCard() {
           </CardContent>
         </Card>
 
-        <Card className="bg-[#13132B] my-8 border-gray-700">
+        <Card className="bg-[#18181B] my-8 border-gray-700">
           <CardContent className="p-4 sm:p-6">
             <div className="flex flex-col sm:flex-row items-center">
-              <img
+              <Image
                 src="/ps/setup.svg"
                 alt="TypeScript logo with glowing effect"
                 className="mb-4 sm:mb-0 sm:mr-6 w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32"
@@ -132,7 +132,7 @@ export default function PsCard() {
               <div>
                 <div className="flex flex-col sm:flex-row items-center sm:items-start mb-2">
                   <h2 className="text-xl sm:text-2xl text-white font-bold mb-2 sm:mb-0 sm:mr-3 text-center sm:text-left">
-                    Applied Digital Skills
+                    Backend Pro Essentials
                   </h2>
                   <span className="bg-green-500 text-black text-xs font-bold px-2 py-1 rounded">
                     NEW
@@ -142,12 +142,12 @@ export default function PsCard() {
                   4 Months Program
                 </p>
                 <p className="text-sm text-gray-300 mb-4 text-center sm:text-left">
-                  A practical guide to learning digital skills. Learn how to use
-                  productivity tools to improve your work delivery and
-                  collaboration.
+                  Total Backend Pro Essentials is the ultimate guide to becoming
+                  a backend developer. Learn how to build backend applications
+                  in a professional way.
                 </p>
                 <div className="flex justify-center sm:justify-start">
-                  <Link href="/programs/digital-skills">
+                  <Link href="/programs/">
                     <Button variant="secondary" size="sm">
                       Learn More →
                     </Button>
